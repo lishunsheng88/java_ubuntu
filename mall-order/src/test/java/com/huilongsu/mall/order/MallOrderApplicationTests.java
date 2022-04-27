9f55ac7ef18f3dfa79088161a9044d4592baf6ba
huilongsu <3148835359@qq.com>
2022-04-27 21:28:26 +0800

    项目初始结构构建

---
+++ mall-order/src/test/java/com/huilongsu/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huilongsu.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
